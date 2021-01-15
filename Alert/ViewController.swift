bef82d3b64a46aac882067d352d4af4678946273
JiHoonAHN <ahnjh1028@naver.com>
2021-01-15 15:10:48 +0900

    Initial Commit

---
+++ Alert/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alert
+//
+//  Created by Ji-hoon Ahn on 2021/01/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
